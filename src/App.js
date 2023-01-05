5b99bed3c0b9e684681f973e79398074c608e83f
Bruno Jacby <brunojck@hotmail.com>
2023-01-05 01:23:37 +0000

    adc button clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [points, setPoints] = useState([]);
+  const [removedPoints, setRemovedPoints] = useState([]);
+
+  const handleClick = (event) => {
+    setPoints([...points, { x: event.clientX, y: event.clientY }]);
+  };
+
+  const handleRedo = () => {
+    if (points.length > 0) {
+      setRemovedPoints([...removedPoints, points[points.length - 1]]);
+      setPoints(points.slice(0, points.length - 1));
+    }
+  };
+
+  const handleUndo = () => {
+    if (removedPoints.length > 0) {
+      setPoints([...points, removedPoints[removedPoints.length - 1]]);
+      setRemovedPoints(removedPoints.slice(0, removedPoints.length - 1));
+    }
+  };
+
+  const handleClear = () => {
+    setPoints([]);
+    setRemovedPoints([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleRedo}>Redo</button>
+      <button onClick={handleUndo}>Undo</button>
+      <button onClick={handleClear}>Clear</button>
+      <div className='App' onClick={handleClick}>
+        {points.length > 0 &&
+          points.map((point, index) => (
+            <div
+              key={index}
+              className="red-circle"
+              style={{
+                position: 'absolute',
+                top: point.y - 5,
+                left: point.x - 5,
+              }}
+            ></div>
+          ))}
+      </div>      
     </div>
   );
-}
+};
 
 export default App;
